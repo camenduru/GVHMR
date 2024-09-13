8d01ff0c06473cd6f767e5824611cb695f5c7951
camenduru <54370274+camenduru@users.noreply.github.com>
2024-09-14 02:48:18 +0300

    test

--- hmr4d/utils/preproc/__init__.py
+++ hmr4d/utils/preproc/__init__.py
@@ -1,7 +1,4 @@
-try:
-    from hmr4d.utils.preproc.tracker import Tracker
-    from hmr4d.utils.preproc.vitfeat_extractor import Extractor
-    from hmr4d.utils.preproc.vitpose import VitPoseExtractor
-    from hmr4d.utils.preproc.slam import SLAMModel
-except:
-    pass
+from hmr4d.utils.preproc.tracker import Tracker
+from hmr4d.utils.preproc.vitfeat_extractor import Extractor
+from hmr4d.utils.preproc.vitpose import VitPoseExtractor
+from hmr4d.utils.preproc.slam import SLAMModel
